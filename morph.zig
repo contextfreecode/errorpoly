300649514d891eb00358d0ef09d22e8456ddf382
Tom <tom@contextfree.info>
2025-07-11 06:09:08 -0700

    Provide args and freeing

--- morph.zig
+++ morph.zig
@@ -12,7 +12,7 @@ pub fn main() error{OutOfMemory}!void {
             defer allocator.free(text);
             std.debug.print("{s}\n", .{text});
         } else |err| switch (err) {
-            error.BadToken => std.debug.print("bad token", .{}),
+            error.BadToken => std.debug.print("bad token\n", .{}),
             else => |other_err| return other_err,
         }
     }
@@ -22,7 +22,7 @@ pub fn binarize(
     allocator: Allocator,
     tokens: []const []const u8,
 ) error{ BadToken, OutOfMemory }![]const u8 {
-    return mapJoin(allocator, tokens, struct {
+    return mapJoin(allocator, tokens, false, struct {
         pub fn parse(token: []const u8) error{BadToken}![]const u8 {
             if (std.mem.eql(u8, token, "yes")) {
                 return "1";
@@ -32,18 +32,24 @@ pub fn binarize(
                 return error.BadToken;
             }
         }
-    }.parse);
+    }.parse, .{});
 }
 
 pub fn mapJoin(
     allocator: Allocator,
     items: anytype,
+    freeing: bool,
     func: anytype,
+    args: anytype,
 ) WithErrType(ReturnType(func), error{OutOfMemory}, []const u8) {
     var buffer = std.ArrayList(u8).init(allocator);
     defer buffer.deinit();
     for (items) |item| {
-        try buffer.appendSlice(try func(item));
+        const text = try @call(.auto, func, .{item} ++ args);
+        if (freeing) {
+            defer allocator.free(text);
+        }
+        try buffer.appendSlice(text);
     }
     return buffer.toOwnedSlice();
 }
